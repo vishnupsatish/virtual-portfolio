a962a7ba6f0f8c0fbc2f15bb4692e46128e50042
vishnupsatish <vishnupavan.satish@gmail.com>
2020-07-12 18:00:52 -0400

    Add Database URL

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
